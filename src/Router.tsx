7e1e6223b3a1ad7e56ee104e109e70dd44e62de1
Mostafa Saffari <mostafasaffari@gmail.com>
2019-12-12 10:40:57 +0330

    chore: add redux provider and connected router to project

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,25 @@
 import React, { Suspense, lazy } from "react";
+import { Provider } from "react-redux";
+import { store, history } from "./pages/redux/store";
+import { ConnectedRouter } from "connected-react-router";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const App = lazy(() => import("./pages/app/App"));
 
 const Router: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Suspense fallback={<div />}>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route component={() => <h1>404</h1>} />
-        </Switch>
-      </Suspense>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Suspense fallback={<div />}>
+          <Switch>
+            <ConnectedRouter history={history}>
+              <Route exact path="/" component={App} />
+              <Route component={() => <h1>404</h1>} />
+            </ConnectedRouter>
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
