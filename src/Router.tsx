84d1f1cf5f0db81d564f95f703ce29a637e48f5a
Mostafa Saffari <mostafasaffari@gmail.com>
2019-12-14 11:41:12 +0330

    chore: implement theme provider

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,22 +3,25 @@ import { Provider } from "react-redux";
 import { store, history } from "./redux/store";
 import { ConnectedRouter } from "connected-react-router";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 const App = lazy(() => import("./pages/app/App"));
 
 const Router: React.FC = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Suspense fallback={<div />}>
-          <Switch>
-            <ConnectedRouter history={history}>
-              <Route exact path="/" component={App} />
-              <Route component={() => <h1>404</h1>} />
-            </ConnectedRouter>
-          </Switch>
-        </Suspense>
-      </BrowserRouter>
+      <ThemeProvider theme={store.getState().AppSetting.theme}>
+        <BrowserRouter>
+          <Suspense fallback={<div />}>
+            <Switch>
+              <ConnectedRouter history={history}>
+                <Route exact path="/" component={App} />
+                <Route component={() => <h1>404</h1>} />
+              </ConnectedRouter>
+            </Switch>
+          </Suspense>
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   );
 };
