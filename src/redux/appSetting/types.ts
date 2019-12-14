8713f0b4cc1ad1dabf565ef5836f9351ea47ef95
Mostafa Saffari <mostafasaffari@gmail.com>
2019-12-14 11:42:34 +0330

    chore: add index for theme and special theme for sample

--- src/redux/appSetting/types.ts
+++ src/redux/appSetting/types.ts
@@ -1,14 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
 export const CHANGE_THEME = "CHANGE_THEME";
 
 export interface IAppSettingState {
-  theme: string;
+  theme: DefaultTheme;
 }
 
 interface IChangeTheme {
   type: typeof CHANGE_THEME;
   theme: IAppSettingState["theme"];
 }
-
 
 export interface IAppSettingActions {
   changeTheme: (theme: IAppSettingState["theme"]) => IChangeTheme;
---
+++ src/style/themes/special.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  borderRadius: "8px",
+
+  colors: {
+    blackToWhite: ["#010101", "#fafafafa"],
+    main: ["#ff0000"]
+  }
+};
+
+export default theme;
