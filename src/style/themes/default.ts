0034e38b5280a14958c7cbe41b5bad78f2a9baf5
Mostafa Saffari <mostafasaffari@gmail.com>
2019-12-14 11:14:30 +0330

    chore: add defautl theme to project

---
+++ src/style/themes/default.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  borderRadius: "3px",
+
+  colors: {
+    blackToWhite: ["#000000", "#ffffff"],
+    main: ["#1890ff"]
+  }
+};
+
+export default theme;
