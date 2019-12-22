ae35782a4a491960d3510185b866efc104ae64d4
Mostafa Saffari <mostafasaffari@gmail.com>
2019-12-22 09:14:07 +0330

    chore: add side bar and appbar to project

--- src/pages/app/RestrictedRouter.tsx
+++ src/pages/app/RestrictedRouter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import Home from "./home/Home";
+import Home from "./home";
 
 interface IProps {
   url: string;
--- src/pages/app/layout/Layout.tsx
+++ src/pages/app/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouteComponentProps } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
 import { ThemeProvider, DefaultTheme } from "styled-components";
@@ -6,27 +6,45 @@ import { ThemeProvider, DefaultTheme } from "styled-components";
 import { AppState } from "../../../redux/store";
 import appSettingActions from "../../../redux/appSetting/actions";
 
-import ChangeTheme from "../../../components/changeTheme/changeTheme";
+import SideBar from "../../../components/sideBar";
+import Layout from "../../../components/ui-kit/layout";
+import ChangeTheme from "../../../components/changeTheme";
 
 import AppRouter from "../RestrictedRouter";
 
 import AppWrapper from "./layout.style";
+import AppBar from "../../../components/appBar/appBar";
 
 interface IProps extends RouteComponentProps {}
 const App: React.FC<IProps> = ({ match }) => {
+  const [collapseSideBar, setCollapseSideBar] = useState(false);
   const theme = useSelector((state: AppState) => state.AppSetting.theme);
   const dispatch = useDispatch();
   const { url } = match;
   const handleChangeTheme = (theme: DefaultTheme) => {
     dispatch(appSettingActions.changeTheme(theme));
   };
+  const handleToggleSideBar = (
+    event: React.MouseEvent<HTMLElement, MouseEvent>
+  ) => {
+    setCollapseSideBar(!collapseSideBar);
+  };
   return (
     <ThemeProvider theme={theme}>
-      <ChangeTheme onChangeTheme={handleChangeTheme} />
-      <AppWrapper data-test="wrapper">
-        <h1>Test layout </h1>
-        <AppRouter url={url} />
-      </AppWrapper>
+      <Layout>
+        <SideBar
+          url={url}
+          title="Dashboard"
+          collapsed={collapseSideBar}
+        />
+        {/* <ChangeTheme onChangeTheme={handleChangeTheme} /> */}
+        <Layout>
+          <AppBar collapsed={collapseSideBar} toggle={handleToggleSideBar} />
+          <AppWrapper data-test="wrapper">
+            <AppRouter url={url} />
+          </AppWrapper>
+        </Layout>
+      </Layout>
     </ThemeProvider>
   );
 };
