7de825d7ef610dad46eda51dd2bb3393f1c45f9a
Mostafa Saffari <mostafasaffari@gmail.com>
2020-01-13 09:22:30 +0330

    chore: add interface api config for api helper

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,3 +1,12 @@
 import axios from "axios";
 
+import handleError from "./handleApiError";
+
+interface IApiConfig {
+  baseURL?: string;
+  headers?: {
+    "X-TOKEN"?: string | null;
+  };
+}
+
 export { axios };
